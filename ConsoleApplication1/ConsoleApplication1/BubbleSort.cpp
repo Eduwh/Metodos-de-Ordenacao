f791c08e9f48b362b133cc1381713f58d584940d
Eduwh <eduwh.lucrezia@gmail.com>
2018-09-10 19:41:00 -0300

    Rename ConsoleApplication1.cpp to BubbleSort.cpp

---
+++ ConsoleApplication1/ConsoleApplication1/BubbleSort.cpp
@@ -0,0 +1,13 @@
+﻿void bubbleSort(int vet[], int length) {
+	int i, j, temp;
+	for (i = 0; i < length - 1; i++) {
+		for (j = (i + 1); j < length; j++) {
+			if (vet[j] < vet[i]) {
+				temp = vet[i];
+				vet[i] = vet[j];
+				vet[j] = temp;
+			}
+		}
+
+	}
+}
