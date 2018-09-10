256c71b0c6359fdcb480d1d0e00ccf9956243e43
Eduardo Cardoso <eduardo.cardoso@edu.univali.br>
2018-09-10 19:51:23 -0300

    master

---
+++ ConsoleApplication1/ConsoleApplication1/SelectionSort.cpp
@@ -0,0 +1,13 @@
+void SelectionSort(int vetor[], int tam) {
+	for (int indice = 0; indice < tam; ++indice) {
+		int indiceMenor = indice;
+		for (int indiceSeguinte = indice + 1; indiceSeguinte < tam; ++indiceSeguinte) {
+			if (vetor[indiceSeguinte] < vetor[indiceMenor]) {
+				indiceMenor = indiceSeguinte;
+			}
+		}
+		int aux = vetor[indice];
+		vetor[indice] = vetor[indiceMenor];
+		vetor[indiceMenor] = aux;
+	}
+}
